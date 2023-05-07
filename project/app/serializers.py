d0ed2a5b4c4723d14a549592386be5a23d153352
محمد علي محمد <you@example.com>
2023-05-07 18:16:08 +0300

    adding classes to serializers

--- project/app/serializers.py
+++ project/app/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Publication, Project
+from .models import Publication, Project, SavedItem, News, Notification
+
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -19,3 +20,20 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ['id', 'title', 'description', 'start_date', 'end_date']
+
+class SavedItemSerializer(serializers.ModelSerializer):
+    project = ProjectSerializer(read_only=True)
+    class Meta:
+        model = SavedItem
+        fields = '__all__'
+
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = '__all__'
+
+class NotificationSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    class Meta:
+        model = Notification
+        fields = '__all__'
